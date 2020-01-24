2fa42283b09b96ef5612628f57f19c79069e1561
vital-76 <56651573+vital-76@users.noreply.github.com>
2020-01-24 23:30:56 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,9 @@
+new WOW().init();
+var mySwiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.progects-pagination',
+          bulletClass:'progects-bulet',
+          bulletActiveClass:'progects-bulet-active',
+          clickable: true
+        },
+});
